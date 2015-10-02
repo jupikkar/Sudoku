9bfe3f1163f86ad0cb40876d533c0d355edebec9
somename <someemail>
2015-10-02 12:45:39 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -15,7 +15,7 @@ public class TestSudokuVerifier {
 	}
 	
 	@Test
-	public void testIfFirstCorrectRowIsValid() {
+	public void testIfFirstRowsAreValid() {
 		
 		SudokuVerifier sudoku = new SudokuVerifier(); 
 		String correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
