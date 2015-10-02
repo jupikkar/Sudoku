bbc2ea0d04bcc67ba6aac55c3d7dc52c3baa98bb
somename <someemail>
2015-10-02 12:50:14 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -15,7 +15,7 @@ public class TestSudokuVerifier {
 	}
 	
 	@Test
-	public void testIfFirstRowsAreValid() {
+	public void testIfAllRowsAreValid() {
 		
 		SudokuVerifier sudoku = new SudokuVerifier(); 
 		String correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
