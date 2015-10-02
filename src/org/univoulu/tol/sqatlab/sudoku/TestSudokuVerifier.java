d6138da7fcaf981558c8af6700492c9129ccb601
somename <someemail>
2015-10-02 12:53:57 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -15,11 +15,11 @@ public class TestSudokuVerifier {
 	}
 	
 	@Test
-	public void testIfAllRowsAreValid() {
+	public void testIfAllRowsAreInalidWithIncorrectString() {
 		
 		SudokuVerifier sudoku = new SudokuVerifier(); 
-		String correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		assertEquals(0, sudoku.verify(correctString));
+		String incorrectString = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		assertEquals(0, sudoku.verify(incorrectString));
 	}
 
 }
